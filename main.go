00b487184032e9adb9b18e55913d5fd4092a4253
ecoarchie <eco.artur@gmail.com>
2023-10-14 12:58:35 +0300

    initial commit: boilerplate for http server done

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		log.Fatal("PORT is not found in .env")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/error", handlerError)
+
+	router.Mount("/v1", v1Router)
+
+	fmt.Printf("Server start on port %s\n", portStr)
+	err := http.ListenAndServe(":" + portStr, router)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	fmt.Println("Port is ", portStr)
+}
